3b43ace2636bfd0365791f2f298ecc0d53feef56
YoungSub <64890861+young-cod@users.noreply.github.com>
2024-06-12 22:35:31 +0900

    exercise

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+console.dir(document);
 // document.body.children[1].children[0].href = "https://google.com";
 
 let anchorElement = document.getElementById("external-link");
-anchorElement.href = "https://google.com";
+// anchorElement.href = "https://google.com";
+
+anchorElement = document.querySelectorAll("a");
+anchorElement.href = "https://academind.com";
+
+document.createElement("a");
