59be9e850aa621cffaaa1890f8f69f3e9d25f944
YoungSub <64890861+young-cod@users.noreply.github.com>
2024-06-13 19:43:49 +0900

    요소 삭제

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ let anchorElement = document.getElementById("external-link");
 anchorElement = document.querySelectorAll("a");
 anchorElement.href = "https://academind.com";
 
+//요소 추가하기
 let newAnchorElem = document.createElement("a");
 newAnchorElem.href = "https://google.com";
 newAnchorElem.textContent = "Google";
 
 let firstPara = document.querySelector("p");
 firstPara.append(newAnchorElem);
+
+//요소 삭제하기
+let firstH1Elem = document.querySelector("h1");
+// firstH1Elem.remove();
+firstH1Elem.parentElement.removeChild(firstH1Elem);
