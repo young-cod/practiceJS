5c2f957db5613e68c13604a15418a4ea6cb99c21
YoungSub <64890861+young-cod@users.noreply.github.com>
2024-06-13 19:47:14 +0900

    요소 이동

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let anchorElement = document.getElementById("external-link");
 anchorElement = document.querySelectorAll("a");
 anchorElement.href = "https://academind.com";
 
-//요소 추가하기
+//1. 요소 추가하기
 let newAnchorElem = document.createElement("a");
 newAnchorElem.href = "https://google.com";
 newAnchorElem.textContent = "Google";
@@ -15,7 +15,14 @@ newAnchorElem.textContent = "Google";
 let firstPara = document.querySelector("p");
 firstPara.append(newAnchorElem);
 
-//요소 삭제하기
+//2. 요소 삭제하기
 let firstH1Elem = document.querySelector("h1");
+
+//구버전에서 사용 불가
 // firstH1Elem.remove();
+
+//구버전에서 사용가능
 firstH1Elem.parentElement.removeChild(firstH1Elem);
+
+//3. 요소 이동
+firstPara.parentElement.append(firstPara);
