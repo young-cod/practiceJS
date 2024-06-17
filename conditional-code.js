b804c41d6d54546240c996104e33af415b1421de
YoungSub <64890861+young-cod@users.noreply.github.com>
2024-06-17 17:03:10 +0900

    for문

--- conditional-code.js
+++ conditional-code.js
@@ -9,3 +9,9 @@ let isLoggedIn = true;
 if (!isLoggedIn) {
   console.log("User is NOT logged in!");
 }
+
+const enteredUserName = "YoungSub";
+
+if (enteredUserName) {
+  console.log("Input is valid!");
+}
---
+++ loops.js
@@ -0,0 +1,21 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+const users = ["max", "anna", "joel"];
+
+console.log(users);
+
+for (const user of users) {
+  console.log(user);
+}
+
+const loggedInUser = {
+  name: "sub",
+  age: 30,
+  isAdmin: true,
+};
+
+for (const info in loggedInUser) {
+  console.log(loggedInUser[info]);
+}
